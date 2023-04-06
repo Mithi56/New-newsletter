e1f2af54b3ad3b3b2c8b1bda11271f123ed60510
shajia parveen mithila <mithila@shajias-Air.netis>
2023-04-07 01:18:07 +0600

    first commit

--- app.js
+++ app.js
@@ -37,14 +37,14 @@ app.post("/" , function(req , res){
     const url = "https://us21.api.mailchimp.com/3.0/lists/a22118e3e2";
     const options ={
         method: "POST",
-        auth: "shajia1:6869ed5a29de794a821ade4f8ecb6688-us21"
+        auth: "shajia1:d3c43c99e3ca33a629238309b5a85d9d-us21"
     }
 
     const request = https.request(url , options , function(response){
         if(response.statusCode===200){
-            res.sendFile(__dirname+"/success.html");
+            res.sendFile(__dirname + "/success.html");
         }else{
-            res.sendFile(__dirname+"/failure.html");
+            res.sendFile(__dirname + "/failure.html");
         }
         response.on("data" ,function(data){
             console.log(JSON.parse(data));
